4c38a3cc57bd690687af9d86c18f3619382c9a8e
Николай Химич <30412223+Kiuh@users.noreply.github.com>
2023-09-26 16:01:44 +0300

    FirstLabDone

---
+++ Boolib/app/src/main/java/by/bsuir/khimich/boolib/BoolibApplication.kt
@@ -0,0 +1,15 @@
+package by.bsuir.khimich.boolib
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+class BoolibApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidContext(applicationContext)
+            modules()
+        }
+    }
+}
